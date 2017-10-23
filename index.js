e7afbb41e58b6fcb1275834149632c9a2418cd7e
roymeral <33003236+roymeral@users.noreply.github.com>
2017-10-23 23:45:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.ios.js  place code here for Ios
+
+// Import a liabrary to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+
+//Create a component
+
+const App = () => (
+    <Header />
+  );
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
