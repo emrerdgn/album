f95415965a31d60d92bba08c27b2e8dfc5f84802
roymeral <33003236+roymeral@users.noreply.github.com>
2017-10-23 23:45:48 +0300

    Create header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+//Import Liabraries for component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make the component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+// Make the component available to other parts of App
+
+export default Header;
